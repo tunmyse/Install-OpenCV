65d9a94ab6aca460b3b9a918bd2a0c44299e57dd
Luis Arias <kaaloo@gmail.com>
2014-01-03 15:34:21 +0100

    Added runtime dependencies for the fat java lib.

--- Ubuntu/2.4/opencv2_4_8_java.sh
+++ Ubuntu/2.4/opencv2_4_8_java.sh
@@ -9,7 +9,7 @@ mkdir OpenCV
 cd OpenCV
 echo "Removing any pre-installed ffmpeg and x264"
 sudo apt-get -y remove ffmpeg x264 libx264-dev
-echo "Installing Dependenices"
+echo "Installing Dependencies"
 sudo apt-get -y install libopencv-dev
 sudo apt-get -y install build-essential checkinstall cmake pkg-config yasm
 sudo apt-get -y install libtiff4-dev libjpeg-dev libjasper-dev
@@ -30,7 +30,7 @@ make -j4
 sudo make install
 sudo sh -c 'echo "/usr/local/lib" > /etc/ld.so.conf.d/opencv.conf'
 sudo ldconfig
-echo "Removing Dependenices"
+echo "Removing Build Dependencies"
 sudo apt-get -y remove libopencv-dev
 sudo apt-get -y remove build-essential checkinstall cmake pkg-config yasm
 sudo apt-get -y remove libtiff4-dev libjpeg-dev libjasper-dev
@@ -39,4 +39,6 @@ sudo apt-get -y remove libtbb-dev
 sudo apt-get -y remove libqt4-dev libgtk2.0-dev
 sudo apt-get -y remove libfaac-dev libmp3lame-dev libopencore-amrnb-dev libopencore-amrwb-dev libtheora-dev libvorbis-dev libxvidcore-dev
 sudo apt-get -y remove x264 v4l-utils ffmpeg
+echo "Installing Runtime Dependencies"
+sudo apt-get -y install libtbb2 libtiff4 libopenexr6 libgstreamer-plugins-base0.10-0 libv4l-0 libavcodec53 libavformat53 libswscale2 libqt4-opengl libqt4-test
 echo "OpenCV 2.4.8 ready to be used"
